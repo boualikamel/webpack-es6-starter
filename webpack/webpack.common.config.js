4a9af21c36b2bd2d73ba50325de8ac73a488c84f
Bouali <bouali.lemak@gmail.com>
2019-06-27 15:54:05 +0100

    first commit

---
+++ webpack/webpack.common.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    module: {
+        rules: [
+            {
+                test: [/.js$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            },
+            {
+                test: [/.css$|.scss$/],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    'sass-loader',
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/images'
+                        }
+                    },
+
+                ]
+            },
+            {
+                test: /\.(wsv|ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: [
+                  {
+                    loader: "file-loader",
+                    options: {
+                      name: "build/[name].[ext]"
+                    }
+                  }
+                ]
+              },
+        
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.[chunkhash].css'
+        }),
+        new HtmlWebpackPlugin({
+            title: 'lifadmin',
+            template: './src/index.html',
+            chunksSortMode: 'none',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/assets/images',
+            to: 'assets/images'
+        }]),
+
+
+    ]
+ 
+
+
+}
