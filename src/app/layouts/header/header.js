93a78337b2c97370efec3446469ca2ebde3b2978
kamel bouali <bouali.kamel@grp-sonelgaz.sng>
2020-01-15 09:13:14 +0100

    adding some sass code

--- src/app/layouts/header/header.js
+++ src/app/layouts/header/header.js
@@ -5,11 +5,10 @@ class Header {
   loadHeader() {
     // Creata a new instance
     const profile = new Profile(); 
-    
     // Invoke the method (component)
     profile.loadProfile();
     
-    document.body.innerHTML = template;
+    document.body.insertAdjacentHTML('beforeend', template);
     
     // Output loading status
     console.log('HEADer component is loaded...')
--- src/app/layouts/main/main.js
+++ src/app/layouts/main/main.js
@@ -1,5 +1,9 @@
+import template from './main.html';
 class Main {
   loadMain() {
+    
+    document.body.insertAdjacentHTML('beforeend', template);
+    
     // Output loading status
     console.log('Main component is loaded...')
   }
